1d13edce98e4bde1e83a674138d029f11c691f87
Wes <westreet7@outlook.com>
2023-06-12 13:54:07 +0800

    update

--- CAnswer/16-30/22.c
+++ CAnswer/16-30/22.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    
+    int hour,minutes;
+    scanf("%d:%d",&hour,&minutes);
+    if (hour>=12)
+    {
+        if(hour==12&&minutes==0)
+        printf("%02d:%02d AM",hour,minutes);
+        else
+        printf("%02d:%02d PM",hour-12,minutes);
+    }
+    else
+    {
+        printf("%02d:%02d AM",hour,minutes);
+    }
     return 0;
 }
